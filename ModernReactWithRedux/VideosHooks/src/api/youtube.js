426fe117921fea3bf0a9f1d6c522a12ee8dc2c3f
Vinicius Rezende <rezendedev@gmail.com>
2022-09-14 12:14:58 -0300

    Refactor with Hooks.

---
+++ ModernReactWithRedux/VideosHooks/src/api/youtube.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const key = "invalid key";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    type: "video",
+    maxResults: 5,
+    key: key,
+  },
+});
