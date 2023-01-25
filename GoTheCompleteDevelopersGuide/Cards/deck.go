1110131f6cd0240945fc887f114a2cd80d88b11d
Rezende <rezendedev@gmail.com>
2023-01-24 21:50:38 -0300

    wip: Cards lecture.

--- GoTheCompleteDevelopersGuide/Cards/deck.go
+++ GoTheCompleteDevelopersGuide/Cards/deck.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+) 
 
 type deck []string
 
@@ -8,4 +13,42 @@ func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	//0666 means anyone can read/write this file
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string {"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value + " of " + suit )
+		}
+	}
+
+	return cards
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",");
+	return deck(s);
 }
--- GoTheCompleteDevelopersGuide/Cards/main.go
+++ GoTheCompleteDevelopersGuide/Cards/main.go
@@ -2,12 +2,6 @@ package main
 
 func main() {
 	//var card string = "Ace of Spades"
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
-
+	cards := newDeckFromFile("my_cards")
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
