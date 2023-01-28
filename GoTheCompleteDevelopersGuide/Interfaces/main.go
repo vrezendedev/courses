0a6b8edbcd7a779569d14fa8deb428992de513a8
Rezende <rezendedev@gmail.com>
2023-01-28 14:51:07 -0300

    Basics about Interfaces.

---
+++ GoTheCompleteDevelopersGuide/Interfaces/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	return "Hi there!"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola!"
+}
+
+/*
+	Interfaces are not generic types.
+	Interfaces are implicit.
+*/
